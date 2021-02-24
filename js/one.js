a0ded6dfd4d6eee9b6ff8bcfd6a668ca63ce5c34
avinash <avinashkumar.officialmail@gmail.com>
2021-02-24 11:08:15 +0530

    added finishing touch for v1

---
+++ js/one.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $("#flip").click(function () {
+        $("#panel").slideToggle("fast");
+    });
+    $("#flip2").click(function () {
+        $("#panel2").slideToggle("fast");
+    });
+    $("#flip3").click(function () {
+        $("#panel3").slideToggle("fast");
+    });
+    $("#flip4").click(function () {
+        $("#panel4").slideToggle("fast");
+    });
+    $("#flip5").click(function () {
+        $("#panel5").slideToggle("fast");
+    });
+    $("#flip6").click(function () {
+        $("#panel6").slideToggle("fast");
+    });
+    $("#flip7").click(function () {
+        $("#panel7").slideToggle("fast");
+    });
+    $("#flip8").click(function () {
+        $("#panel8").slideToggle("fast");
+    });
+    $("#flip9").click(function () {
+        $("#panel9").slideToggle("fast");
+    });
+    $("#flip10").click(function () {
+        $("#panel10").slideToggle("fast");
+    });
+});
